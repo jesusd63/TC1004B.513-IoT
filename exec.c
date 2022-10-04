da57794743997b27af8d1e149a49312f90a23529
jesusd63 <A00227255@itesm.mx>
2022-10-04 13:52:14 +0000

    Practica 1

--- exec.c
+++ exec.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(){
+    printf("Prueba\n");
     int pid = fork();
 
     if(pid == 0){
-        printf("Soy el proceso hijo y me voy a convertir en ls\n");
-        execl("/usr/bin/ls", "ls", "/etc", NULL);
+        printf("Soy el proceso hijo y me voy a convertir en hola\n");
+        execl("/workspace/TC1004B.513-IoT/hola", "holamundo", "/etc", NULL);
         printf("Esto no debe ejecutarse");
     }else{
         printf("Soy el proceso padre\n");
